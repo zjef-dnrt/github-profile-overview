f3b91d0692bd2344de12118bb95eb7d9b3c6d055
Tjeef <zjef96@hotmail.com>
2022-05-19 17:24:41 +0200

    style: remove red test bg update header styles

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,7 +3,7 @@
     <header class="h-16 bg-gray-50 flex px-5 items-center border-b-2 border-gray-400">
       <h1 class="text-gray-800">Github profile overview</h1>
     </header>
-    <main class="h-full w-auto bg-red-100">
+    <main class="h-full w-auto">
       <Nuxt />
     </main>
   </div>
