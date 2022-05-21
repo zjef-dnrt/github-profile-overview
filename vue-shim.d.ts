d963ba2294fd289e6763f4ad632bbbd578d8478a
Tjeef <zjef96@hotmail.com>
2022-05-21 12:48:13 +0200

    tests: setup tests and added tests for searchtoolbar

--- vue-shim.d.ts
+++ vue-shim.d.ts
@@ -1,12 +1,10 @@
 // eslint-disable-next-line import/named
 import { NuxtAxiosInstance } from '@nuxtjs/axios'
-import Vue from 'vue'
 
 declare module '*.vue' {
+  import Vue from 'vue'
   export default Vue
 }
-
-declare module 'nuxt-js/axios'
 
 declare module '@nuxt/types' {
   interface Context {
