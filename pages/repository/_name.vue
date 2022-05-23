1d7200181bc7df10ef5701975dc3c6d6df667580
Tjeef <zjef96@hotmail.com>
2022-05-23 22:51:06 +0200

    feature: commits timeline has infinite scroll

--- pages/repository/_name.vue
+++ pages/repository/_name.vue
@@ -4,8 +4,8 @@
     <p v-else-if="$fetchState.error">An error occurred :(</p>
     <div v-else>
       <h1 class="text-gray-200 tracking-wide">{{ fullName }}</h1>
+      <button class="button--light w-56 my-4" @click="$fetch">Refresh</button>
       <VerticalCommitsTimeline :commits="commits" />
-      <button @click="$fetch">Refresh</button>
     </div>
   </div>
 </template>
@@ -19,12 +19,50 @@ export default Vue.extend({
   asyncData({ params: { fullName } }) {
     return { fullName }
   },
-  async fetch() {
-    const { data } = await this.$reposAPI.get(`${this.fullName}/commits`)
-    this.commits = data
+  data() {
+    return {
+      pageNumber: 1,
+    }
+  },
+  fetch() {
+    this.fetchCommits()
+    this.pageNumber = 1
   },
   computed: {
     ...mapWritableState(useCommitsStore, ['commits']),
+  },
+  beforeMount() {
+    window.addEventListener('scroll', this.checkScrollPosition)
+  },
+  beforeDestroy() {
+    window.removeEventListener('scroll', this.checkScrollPosition)
+  },
+  methods: {
+    fetchMoreCommits() {
+      this.pageNumber += 1
+      this.fetchCommits(this.pageNumber)
+    },
+    async fetchCommits(page: Number = 1) {
+      const { data } = await this.$reposAPI.get(`${this.fullName}/commits`, {
+        params: {
+          per_page: 20,
+          page,
+        },
+      })
+
+      if (page === 1) this.commits = data
+      else this.commits.push(...data)
+    },
+    checkScrollPosition() {
+      // Amount scrolled from top
+      const scrollY = window.scrollY
+      // Visible part of the screen that the user can see
+      const innerHeight = window.innerHeight
+      // Total scrollable document height
+      const scrollHeight = document.documentElement.scrollHeight
+
+      if (scrollY + innerHeight >= scrollHeight) this.fetchMoreCommits()
+    },
   },
 })
 </script>
