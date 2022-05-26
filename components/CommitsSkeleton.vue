03b7900665a81b5ccbc4511a8cbcf67f2397d3b5
Tjeef <zjef96@hotmail.com>
2022-05-26 16:10:40 +0200

    feature: updated commits page skeleton with search

--- components/CommitsSkeleton.vue
+++ components/CommitsSkeleton.vue
@@ -1,69 +1,94 @@
 <template>
-  <ol
-    class="
-      relative
-      ml-7
-      mt-7
-      border-l border-gray-200
-      dark:border-gray-700
-      transition-all
-      duration-1000
-    "
-  >
-    <li v-for="index in [1, 2]" :key="index" class="mb-20 ml-4">
-      <div
-        class="
-          absolute
-          w-3
-          h-3
-          bg-gray-200
-          rounded-full
-          mt-1.5
-          -left-1.5
-          border border-white
-        "
-      ></div>
-      <vue-skeleton-loader
-        type="rect"
-        :width="160"
-        :height="25"
-        rounded
-        animation="wave"
-      />
-      <div class="mt-2 mb-3 flex items-center">
+  <div>
+    <vue-skeleton-loader
+      type="rect"
+      :width="600"
+      :height="40"
+      rounded
+      animation="wave"
+    />
+    <vue-skeleton-loader
+      type="rect"
+      :width="400"
+      :height="40"
+      rounded
+      class="mt-4"
+      animation="wave"
+    />
+    <vue-skeleton-loader
+      type="rect"
+      :width="250"
+      :height="40"
+      rounded
+      class="my-4"
+      animation="wave"
+    />
+    <ol
+      class="
+        relative
+        ml-7
+        mt-7
+        border-l border-gray-200
+        dark:border-gray-700
+        transition-all
+        duration-1000
+      "
+    >
+      <li v-for="index in [1, 2]" :key="index" class="mb-20 ml-4">
+        <div
+          class="
+            absolute
+            w-3
+            h-3
+            bg-gray-200
+            rounded-full
+            mt-1.5
+            -left-1.5
+            border border-white
+          "
+        ></div>
         <vue-skeleton-loader
-          type="circle"
-          :width="50"
-          :height="50"
+          type="rect"
+          :width="160"
+          :height="25"
+          rounded
+          animation="wave"
+        />
+        <div class="mt-2 mb-3 flex items-center">
+          <vue-skeleton-loader
+            type="circle"
+            :width="50"
+            :height="50"
+            animation="wave"
+          />
+          <vue-skeleton-loader
+            type="rect"
+            :width="200"
+            :height="25"
+            class="ml-4"
+            rounded
+            animation="wave"
+          />
+        </div>
+        <vue-skeleton-loader
+          type="rect"
+          :width="350"
+          :height="25"
+          class="mt-5"
+          rounded
           animation="wave"
         />
         <vue-skeleton-loader
           type="rect"
-          :width="200"
-          :height="25"
-          class="ml-4"
+          :width="150"
+          :height="30"
+          class="mt-4"
           rounded
           animation="wave"
         />
-      </div>
-      <vue-skeleton-loader
-        type="rect"
-        :width="350"
-        :height="25"
-        class="mt-5"
-        rounded
-        animation="wave"
-      />
-      <vue-skeleton-loader
-        type="rect"
-        :width="150"
-        :height="30"
-        class="mt-4"
-        rounded
-        animation="wave"
-      />
-    </li>
-  </ol>
+      </li>
+    </ol>
+  </div>
 </template>
 
 <script lang="ts">
